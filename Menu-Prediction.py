46b75873cad3cb1528c0ec6cb826f8c02626e863
Ervin-Magi <61249211+Ervin-Magi@users.noreply.github.com>
2023-01-17 15:45:11 +0200

    Update Menu-Prediction.py

--- Menu-Prediction.py
+++ Menu-Prediction.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 # Load the data into a pandas DataFrame
 data = pd.DataFrame([['burger', 'Monday', 10, 20, 4, 50],
